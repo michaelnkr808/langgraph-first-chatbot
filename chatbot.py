dbab28fa3184d25bee66f1a3b3e72486553d1025
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-10-03 15:26:47 -0700

    Add chat prompting and agent response in cli

--- chatbot.py
+++ chatbot.py
@@ -24,3 +24,21 @@ graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
 graph = graph_builder.compile()
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye")
+            break
+        stream_graph_updates(user_input)
+    except:
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
